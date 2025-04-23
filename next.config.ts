baa4a80f38062d550554d68adaef29ca150b4d89
anuvasamanta <samantaanuva1@gmail.com>
2025-04-24 02:45:11 +1030

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,7 @@
-// import type { NextConfig } from "next";
-
-import { NextConfig } from "next"
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-// import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Build settings
-  output: 'standalone', // or 'export' for static sites
-  distDir: '.next', // default build directory
-  productionBrowserSourceMaps: true, // generate source maps
-  typescript: {
-    ignoreBuildErrors:true, // set true to bypass TS errors during build
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // set false to enforce ESLint during build
-  },
-  images: {
-    domains: ['example.com'], // allowed image domains
-  },
-  
-}
+  /* config options here */
+};
 
-export default nextConfig
+export default nextConfig;
